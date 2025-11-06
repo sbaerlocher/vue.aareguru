5698131485b3d002f51c6d6426b789ad584ca40d
Simon Bärlocher <s.baerlocher@sbaerlocher.ch>
2025-11-06 23:00:31 +0100

    chore: update package.json to set module type and remove eslint config

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  {
+    ignores: [
+      'dist/',
+      'dist-demo/',
+      'coverage/',
+      'node_modules/',
+      '**/*.config.js',
+      '**/*.config.ts',
+      '**/*.config.mts',
+      'tests/',
+      '**/*.spec.ts',
+      '**/*.test.ts',
+      '**/*.d.ts',
+      '.vite/',
+      '.cache/'
+    ]
+  },
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue', '**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      },
+      globals: {
+        node: true,
+        es2020: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {}
+  }
+]
